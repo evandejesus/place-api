a4a5fef40c285eb295d2f91fade1084f61263c97
Evan de Jesus <evanjdejesus@gmail.com>
2022-10-02 15:46:12 -0600

    add get canvas

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"context"
+	"errors"
+	"fmt"
 	"net/http"
 
 	"github.com/joho/godotenv"
@@ -24,6 +26,10 @@ import (
 
 var mongoDB *mongo.Client
 var redisCache *redis.Client
+
+const (
+	CANVAS_SIZE = 10
+)
 
 type Square struct {
 	X         int    `bson:"x"`
@@ -48,6 +54,7 @@ func main() {
 	router.GET("/squares", getSquares)
 	router.GET("/square", getSquareByLocation)
 	router.PUT("/squares", middleware.CheckRateLimit(), putSquare)
+	router.GET("/canvas", getCanvas)
 
 	// DB
 	mongoDB = db.GetMongoClient(ctx)
@@ -58,10 +65,6 @@ func main() {
 
 // getSquares responds with the list of all squares as JSON.
 func getSquares(c *gin.Context) {
-	// ctx := context.Background()
-	// redisCache.Set(ctx, "key", "val2", 0)
-	// val, _ := redisCache.Get(ctx, "key").Result()
-	// log.Println(val)
 
 	// find all squares
 	collection := db.Squares(mongoDB)
@@ -79,10 +82,23 @@ func getSquares(c *gin.Context) {
 	c.JSON(http.StatusOK, results)
 }
 
+// getCanvas returns the whole redis bitfield as a byte array.
+func getCanvas(c *gin.Context) {
+	ctx := context.Background()
+	const ARRAY_LENGTH = CANVAS_SIZE * CANVAS_SIZE
+	bytes, err := redisCache.GetRange(ctx, fmt.Sprintf("squares-%d", CANVAS_SIZE), 0, ARRAY_LENGTH).Bytes()
+	if helpers.Error(c, err) {
+		log.Println(err)
+		return
+	}
+	c.JSON(http.StatusOK, bytes)
+}
+
 // getSquareByLocation returns a single square given X and Y coordinates.
 func getSquareByLocation(c *gin.Context) {
 	X, _ := strconv.Atoi(c.Query("X"))
 	Y, _ := strconv.Atoi(c.Query("Y"))
+
 	collection := db.Squares(mongoDB)
 	filter := bson.D{
 		{Key: "X", Value: X},
@@ -91,24 +107,49 @@ func getSquareByLocation(c *gin.Context) {
 	var square Square
 	err := collection.FindOne(context.TODO(), filter).Decode(&square)
 	if err == mongo.ErrNoDocuments {
-		c.Status(http.StatusNoContent)
+		c.JSON(http.StatusOK, Square{
+			X:     X,
+			Y:     Y,
+			Color: 0,
+		})
+
 		return
 	}
 	c.JSON(http.StatusOK, square)
 
 }
 
+type NewSquare struct {
+	X      int    `json:"x" binding:"required"`
+	Y      int    `json:"y" binding:"required"`
+	Color  byte   `json:"color" binding:"required"`
+	Author string `json:"author" binding:"required"`
+}
+
+func (sq *NewSquare) Validate() error {
+	if sq.Color >= 16 {
+		return errors.New("color must be less than 16")
+	}
+	if sq.X < 0 || sq.Y < 0 {
+		return fmt.Errorf("invalid coordinates: (%d,%d)", sq.X, sq.Y)
+	}
+	return nil
+}
+
 // putSquare creates a square object in the db.
 func putSquare(c *gin.Context) {
+	ctx := context.Background()
 
-	var json struct {
-		X      int    `json:"x" binding:"required"`
-		Y      int    `json:"y" binding:"required"`
-		Color  byte   `json:"color" binding:"required"`
-		Author string `json:"author" binding:"required"`
+	var json NewSquare
+	if err := c.ShouldBind(&json); helpers.Error(c, err) {
+		log.Println(err)
+		return
 	}
 
-	if err := c.Bind(&json); helpers.Error(c, err) {
+	// Validate request
+	if err := json.Validate(); err != nil {
+		c.Error(err)
+		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"status": false, "message": err.Error()})
 		return
 	}
 
@@ -132,14 +173,22 @@ func putSquare(c *gin.Context) {
 		{Key: "Author", Value: json.Author},
 		{Key: "Timestamp", Value: timestamp},
 	}}}
-	result, err := squaresColl.UpdateOne(context.TODO(), filter, update, opts)
+	// upsert square
+	squareResult, err := squaresColl.UpdateOne(ctx, filter, update, opts)
 	if helpers.Error(c, err) {
 		return
 	}
+	log.Debug(squareResult)
+	// create timestamp entry
 	timestampInsert := bson.D{{Key: "Author", Value: json.Author}, {Key: "Timestamp", Value: timestamp}}
-	timestampResult, err := timestampsColl.InsertOne(context.TODO(), timestampInsert)
+	timestampResult, err := timestampsColl.InsertOne(ctx, timestampInsert)
 	if helpers.Error(c, err) {
 		return
 	}
-	c.JSON(http.StatusOK, map[string]interface{}{"squares": result, "timestamps": timestampResult})
+	log.Debug(timestampResult)
+
+	redisResult := redisCache.BitField(ctx, fmt.Sprintf("squares-%d", CANVAS_SIZE), "SET", "u4", fmt.Sprintf("#%d", json.X+CANVAS_SIZE*json.Y), json.Color)
+	log.Debug(redisResult)
+
+	c.JSON(http.StatusOK, map[string]interface{}{"status": true, "message": fmt.Sprintf("user %s successfully inserted color %d at pos (%d,%d)", json.Author, json.Color, json.X, json.Y)})
 }
